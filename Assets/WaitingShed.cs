f4483abb674c9d856f5959d1935fc156e7dce46e
A_Jordan <tontoncabanes@gmail.com>
2026-07-16 15:55:07 +0800

    Revert "Merge branch 'fuck' of https://github.com/dominateswitchcase/daloyMotorwaysFinal into fuck"
    This reverts commit 7aaa524514bb0ffb68f04ca008451abd205e2dac, reversing
    changes made to 12a37470d1399164cea59b0ef882d29729a1f263.

---
+++ Assets/WaitingShed.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class WaitingShed : MonoBehaviour
+{
+    // Existing field used by RouteBuilder
+    public RoadNode roadNode;
+
+    // New fields for placement
+    public RoadSegment road;
+    public float t;
+}
