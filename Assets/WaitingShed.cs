99ab4b6fa109307265e9d4d9bddf4141b8fbe62a
A_Jordan <tontoncabanes@gmail.com>
2026-07-16 14:08:55 +0800

    baja blast

--- Assets/WaitingShed.cs
+++ Assets/WaitingShed.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class WaitingShed : MonoBehaviour
 {
+    // Existing field used by RouteBuilder
+    public RoadNode roadNode;
+
+    // New fields for placement
     public RoadSegment road;
-    public float t;      // 0-1 position along the spline
+    public float t;
 }
